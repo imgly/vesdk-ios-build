62ab29093db7bec21fa433a752d31237ec7a3aec
Alexander Schiewe <alexander.schiewe@img.ly>
2021-01-21 20:36:14 +0100

    Add version 10.21.5

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "VideoEditorSDK",
+  platforms: [.iOS(.v9)],
+  products: [
+    .library(name: "VideoEditorSDK", targets: ["VideoEditorSDK"]),
+  ],
+  dependencies: [
+    .package(name: "ImglyKit", url: "https://github.com/imgly/imglykit-sp.git", .exact("10.21.5"))
+  ],
+  targets: [
+    .target(
+      name: "VideoEditorSDK",
+      dependencies: ["ImglyKit"],
+      path: "Source",
+      publicHeadersPath: ""
+    )
+  ]
+)
